7296e19da63262e34224abcc99a2cadf66a5720e
OGDarkFire <73362077+OGDarkFire@users.noreply.github.com>
2020-11-02 02:14:55 -0300

    Se agrega bbdd
    agregada base de datos y un articulo

---
+++ Encargo/EncargoPagina/EncargoPagina/urls.py
@@ -0,0 +1,26 @@
+"""EncargoPagina URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from gestionPaginas import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+        # localhost:8000/
+    path('pagina1/', views.pagina1),
+    path('pagina2/', views.pagina2),
+    path('pagina3/', views.pagina3),
+]
