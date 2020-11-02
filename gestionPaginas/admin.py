0ccf5ad934a0c4ced7e4cfce4102c344d9e529d8
OGDarkFire <73362077+OGDarkFire@users.noreply.github.com>
2020-11-02 20:15:23 -0300

    paginas web

---
+++ gestionPaginas/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Historial
+from .models import Invocador
+
+# Register your models here.
+admin.site.register(Invocador)
---
+++ gestionPaginas/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GestionpaginasConfig(AppConfig):
+    name = 'gestionPaginas'
